154a26dad8f481aae1fb57affe3411cb0bbc49e8
jkapoor18 <jyotikpr999@gmail.com>
2024-02-16 10:41:43 +0530

    variables

---
+++ Variables.py
@@ -0,0 +1,26 @@
+#python don't have any library to generate variables. Variables automatically created when we assign new values in it.
+
+#Examples 1: string type examples
+Fruits = "orange","Papaya","pineapple","grapes"   #here we define fruits as a varibles where I assign fruits name in it.
+
+#even you can assign diffrent types of values like numbers, boolean value, float type values, string, list,tuple etc
+
+number = 3,7,8,4,6,9
+
+#Rules of Define Variables:-
+'''
+1. variables should be start with alphabet and underscore 
+2. we cann't start varibles with numbers and special charactors like [$<%<#<@<!<?]
+
+Variables Examples:
+
+
+
+
+
+
+
+
+
+
+'''
