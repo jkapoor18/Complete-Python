acf41f4846663d973085c33c71929f1564104d88
jkapoor18 <jyotikpr999@gmail.com>
2024-02-21 11:09:51 +0530

    Question

---
+++ practise_Q.py
@@ -0,0 +1,24 @@
+'''Q-1. What will be the output of the following Python code?
+a=[1,2,3,4,5,6,7,8,9]
+print(a[::2])
+
+
+
+Q2. Write a Python program to Multiply all numbers in the list?
+Q3. Write a Python program to find smallest number in a list?
+Q4. Write a Python program to find largest number in a list?
+Q5. Write a Python program to find second largest number in a list?
+Q6. Write a Python program to find N largest elements from a list?
+Q7. Write a Python program to print even numbers in a list?
+Q8. Write a Python program to print odd numbers in a List?
+Q9. Write a Python program to Remove empty List from List?
+
+Q10. Write a Python program to Count occurrences of an element in a list?
+Q11. Write a Python program to find words which are greater than given length k?
+Q12. Write a Python program for removing i-th character from a string?
+Q13. Write a Python program to split and join a string?
+Q14. Write a Python to check if a given string is binary string or not?
+Q15. Write a Python program to find uncommon words from two Strings?
+
+
+'''
