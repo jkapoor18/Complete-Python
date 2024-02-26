ae3926a5263b4ca35140614f7ac8a6ce6129eec6
jkapoor18 <jyotikpr999@gmail.com>
2024-02-26 08:34:08 +0530

    Ques

--- practise_Q.py
+++ practise_Q.py
@@ -22,3 +22,44 @@ Q15. Write a Python program to find uncommon words from two Strings?
 
 
 '''
+
+
+'''
+Day-2 Question 
+
+1. If a student's score is above 90, print "Excellent," else print "Good."
+2. Check if a number is positive, negative, or zero.
+3. Determine if a year is a leap year or not.
+4. Check if a given string is a palindrome.
+5. If a person's age is above 18, print "Adult," else print "Minor."
+6. Determine the largest among three numbers.
+7. Check if a number is even or odd.
+8. If it's raining, print "Take an umbrella," else print "Enjoy the weather."
+9. If a person's BMI is above 30, print "Obese," else print "Normal weight."
+10. Determine the type of a triangle based on its angles.
+11. Check if a user input number is divisible by both 5 and 7.
+12. Determine eligibility for voting based on age.
+13. Assign a letter grade based on a student's test score.
+14. Check if a given year is a century or not.
+15. If a person has a fever, print "Consult a doctor," else print "Rest at home."
+16. Determine the season based on the month entered.
+17. If a person's salary is above $50,000, print "High income," else print "Low income."
+18. Determine if a given number is a prime number.
+19. Check if a character is a vowel or a consonant.
+20. If a user provides valid credentials, print "Login successful," else print "Invalid credentials."
+21. Determine if two numbers are equal, greater, or less than each other.
+22. If a user has a VIP status, print "Access granted," else print "Regular access."
+23. Check if a given point is inside or outside a rectangle.
+24. If a person has a membership, print "Exclusive benefits," else print "Join now."
+25. Determine the eligibility for a discount based on the purchase amount.
+
+
+
+
+
+
+
+
+
+
+'''
